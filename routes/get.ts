0375fa331183b894668e19974419387a6c9b9dba
akumaru <akumaru.senju@gmail.com>
2023-01-26 22:14:15 +0500

    deno fmt

--- routes/get.ts
+++ routes/get.ts
@@ -3,7 +3,7 @@ import { getOne } from "../models/mod.ts";
 
 app.get("/todo/:id", (req, res) => {
   let data = getOne(req.params.id);
-  if (data.length == 0) {
+  if (!data) {
     res.send({ "message": `Todo with ID ${req.params.id} was not found.` });
   } else res.send(data);
 });
